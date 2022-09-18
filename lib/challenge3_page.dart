557c1a9f16fbe2667d3ffb14fe0ea7f9ba8a7991
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-18 12:21:45 -0500

    Init 5.1

---
+++ lib/challenge3_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Challenge3Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.network(
+            "https://images.pexels.com/photos/931018/pexels-photo-931018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 12.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.5),
+                      color: const Color(0xffF6A521),
+                      child: const Text(
+                        "Travel",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.timer,),
+                        Text("12:23",),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:codigo_init/challenge2_page.dart';
+import 'package:codigo_init/challenge3_page.dart';
 import 'package:codigo_init/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Challenge2Page()
+      home: Challenge3Page()
     );
   }
 }
