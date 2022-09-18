928bec44a6e77ee00a6cd488acaf56d708bf87ef
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-18 11:19:57 -0500

    Init 4

--- lib/challenge2_page.dart
+++ lib/challenge2_page.dart
@@ -1,10 +1,43 @@
-
-
 import 'package:flutter/material.dart';
 
 class Challenge2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.arrow_back,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    "News",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                  ),
+                  Icon(
+                    Icons.file_upload_outlined,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Image.network(
+              "https://images.pexels.com/photos/848612/pexels-photo-848612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
